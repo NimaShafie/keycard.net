24bff41aa8e3dc63d7baf96b98b85be6f93d53ce
Aastha Joshi <157534481+JoshiAastha04@users.noreply.github.com>
2025-11-14 00:55:28 -0800

    feat: Login, signup pages

---
+++ src/Web/KeyCard.Web/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./**/*.razor",
+    "./**/*.html",
+    "./**/*.cshtml",
+    "./**/*.razor.css"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
